e4ebdd78afcf4d5e09a7944e98a8fdb0bb1d617a
webkadiz <webkadiz@gmail.com>
2018-12-20 16:42:30 +0300

    map complete

--- main.js
+++ main.js
@@ -1,67 +1,115 @@
+
+
+class Data {
+    constructor () {
+        this.rows = 18;
+        this.columns = 2;
+        this.rowLength = 40;
+        this.chars = [
+            "[",
+            "]",
+            "{",
+            "}",
+            "'",
+            '"',
+            "!",
+            "<",
+            ">",
+            "_",
+            "@",
+            "-",
+            "?",
+            "(",
+            ")",
+            ";",
+            "+",
+            "#",
+            "^",
+            "&",
+            "*"
+           ];
+           this.words = [['пират', 'сигара', 'человек', 'комната', 'оплата', 'лицензия', 'акация', 'собака', 'кукушка', 'макушка', 'парта', 'сапог', 'перчатка', 'бумажка', 'интерес', 'порт']];
+    }
+    initMap(){
+        this.wordIndex = 0;
+        this.countHex = 0;
+        this.randHex    = ~~(Math.random() * 65536);
+     this.countWords = this.rows - ~~(Math.random() * (5 - 2) + 2);
+     this.offsetWord = ~~(this.rowLength * this.rows / this.countWords);
+     this.posWord = ~~(Math.random() * this.offsetWord)
+
+     for (let i of range(this.columns)) {
+        $(".map").append(`<div class="map__column">
+                  <div class="map__decor"></div>
+                  <div class="map__filed"></div>
+              </div>`);
+      
+        for (let rowIndex of range(this.rows)) {
+            this.rowIndex = rowIndex
+         this.field        = $(".map__filed:last")[0],
+              this.decor = $(".map__decor:last");
+      
+              this.fillMap();
+              
+              //this.field.innerHTML += "<br/>";
+              this.countHex += 4;
+      
+         this.decor.append(
+          `<div>${"0x" + (this.randHex + this.countHex).toString(16).toUpperCase()}</div>`
+         );
+        }
+       }
+      
+       $(".map").append('<div class="map__terminal"></div>');
+       $('.map__terminal').append('<div class="map__enter-wrapper"><i class="icon-angle-right"></i><i class="icon-angle-right"></i><i class="icon-angle-right"></i><div class="map__enter"></div></div>')
+       
+    }
+    fillMap() {
+        for (let k of range(this.rowLength)) {
+    
+            let char = this.chars[~~(Math.random() * this.chars.length)];
+        
+            this.field.innerHTML += `<span class="map__char">${char} <span>`;
+            
+            
+        
+            if(this.rowIndex * this.rowLength + k == this.posWord) {
+                console.log(this.wordIndex);
+                
+                this.field.innerHTML += `<span class="map__char map__word">${this.words[0][this.wordIndex].toUpperCase()}<span>`;
+                this.posWord += this.offsetWord
+                this.wordIndex++;
+            }
+           }
+    }
+}
+
+
+let data = new Data()
+
+data.initMap()
+
+
+
+
+
 function* range(amount) {
  for (let i = 0; i < amount; i++) {
   yield i;
  }
 }
 
-function map(strLength = 40, rows = 12, columns = 2) {
- let countHex   = 0,
-     randHex    = ~~(Math.random() * 65536),
-     countWords = rows - ~~(Math.random() * (5 - 2) + 2),
-     fillChars  = [
-   "[",
-   "]",
-   "{",
-   "}",
-   "'",
-   '"',
-   "!",
-   "<",
-   ">",
-   "_",
-   "@",
-   "-",
-   "?",
-   "(",
-   ")",
-   ";",
-   "+",
-   "#",
-   "^",
-   "&",
-   "*"
-  ];
 
- for (let i of range(columns)) {
-  $(".map").append(`<div class="map__column">
-			<div class="map__decor"></div>
-			<div class="map__filed"></div>
-		</div>`);
 
-  for (let j of range(rows)) {
-   let el        = $(".map__filed:last")[0];
-       countHex += 4;
-   for (let k of range(strLength)) {
-    let char = fillChars[~~(Math.random() * fillChars.length)];
 
-    el.innerHTML += `<span class="map__char">${char}<span>`;
-   }
-   el.innerHTML += "<br/>";
 
-   $(".map__decor:last").append(
-    `<div>${"0x" + (randHex + countHex).toString(16).toUpperCase()}</div>`
-   );
-  }
- }
-
- $(".map").append('<div class="map__terminal"></div>');
-}
-
-map();
 
 $(".map__char").on("mouseenter", e => {
  let audio = new Audio();
- console.log(123);
  audio.src      = "keyboard5.mp3";
  audio.autoplay = true;
+
+ console.log(e);
+ 
+ $('.map__enter').html(e.target.textContent)
 });
